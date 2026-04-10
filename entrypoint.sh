782a8a6e098e1cd3bb389565d3b187e8a119b325
peay904 <gpark904@gmail.com>
2026-04-09 21:26:26 -0400

    Add Jellyfin API key and server URL configuration to environment files

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 TOKEN=$(cat /proc/sys/kernel/random/uuid)
 
 : "${INVITE_BASE_URL:?INVITE_BASE_URL is required}"
+: "${JELLYFIN_SERVER_URL:?JELLYFIN_SERVER_URL is required}"
+: "${JELLYFIN_API_KEY:?JELLYFIN_API_KEY is required}"
 : "${JELLYFIN_URL:?JELLYFIN_URL is required}"
 : "${SITE_URL:?SITE_URL is required}"
 
@@ -12,6 +14,8 @@ SITE_DISPLAY=$(echo "$SITE_URL" | sed 's|^https\?://||')
 
 sed -i \
   -e "s|__TOKEN__|${TOKEN}|g" \
+  -e "s|__JELLYFIN_SERVER_URL__|${JELLYFIN_SERVER_URL}|g" \
+  -e "s|__JELLYFIN_API_KEY__|${JELLYFIN_API_KEY}|g" \
   -e "s|__JELLYFIN_URL__|${JELLYFIN_URL}|g" \
   -e "s|__SITE_URL__|${SITE_URL}|g" \
   -e "s|__SITE_DISPLAY__|${SITE_DISPLAY}|g" \
